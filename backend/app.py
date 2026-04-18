986ba77471c5c303e9a0b2ff93d49405d962374b
amarpkst <amarpkst@gmail.com>
2026-04-18 13:16:32 +0530

    day 12 with psycopg2

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 # ✅ DB CONFIG
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin123@db:5432/mydb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # ✅ Initialize DB
@@ -34,6 +34,9 @@ def handle_exception(e):
         "data": None,
         "error": str(e)
     }, 500
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
